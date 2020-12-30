377d1e4bb6965caa2e6c98e51a231a0895c4fb01
Echo-Mr-Pengw <2943927631@qq.com>
2020-12-30 10:08:57 +0800

    Add links and categories

--- controllers/home/index.go
+++ controllers/home/index.go
@@ -10,12 +10,18 @@ type IndexController struct {
 }
 
 func (c *IndexController) Index() {
-	
+
 	// 个人简介
 	profile,_ := models.GetOneProfile()
+	// 友情正常显示的连接
+	links, _ := models.GetNormalLinks()
+	// 随笔分类
+	tag, _ := models.GetNormalTagsList()
 	// 文章列表
 	articleList,_ := models.GetArticleList()
 
+	c.Data["links"]       = links
+	c.Data["tag"]         = tag
 	c.Data["profile"]     = profile
 	c.Data["articleList"] = articleList
 	c.TplName = "home/index.html"
